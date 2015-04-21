a67c722c274dddd85f084404bf14093bac11d6a7
tyage <namatyage@gmail.com>
2015-04-21 15:34:26 +0900

    to_f

--- logger.rb
+++ logger.rb
@@ -10,9 +10,10 @@ end
 
 realtime = Slack.realtime
 realtime.on :message do |mes|
+  p mes
   log = SlackLog.new
   log.text = mes['text']
-  log.posted_at = mes['ts']
+  log.posted_at = mes['ts'].to_f
   log.channel = mes['channel']
   log.user = mes['user']
   log.save
