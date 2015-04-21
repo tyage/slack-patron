185bb87eb31b4d1b83355818e3114d4e5b797420
tyage <namatyage@gmail.com>
2015-04-21 18:15:36 +0900

    member, channelを取得

--- viewer.rb
+++ viewer.rb
@@ -3,10 +3,31 @@ require './slack'
 require './db'
 
 def members
-  Slack.users_list
+  hashed_members = {}
+  Slack.users_list['members'].each do |m|
+    hashed_members[m['id']] = m
+  end
+  hashed_members
+end
+
+def channels
+  hashed_channels = {}
+  Slack.channels_list['channels'].each do |c|
+    hashed_channels[c['id']] = c
+  end
+  hashed_channels
 end
 
 get '/' do
+  hashed_logs = {}
   logs = SlackLog.all
-  erb :index, locals: { logs: logs, members: members['members'] }
+  logs.each do |l|
+    hashed_logs[l.channel] ||= []
+    hashed_logs[l.channel] << l
+  end
+  erb :index, locals: {
+    logs: hashed_logs,
+    members: members,
+    channels: channels
+  }
 end
