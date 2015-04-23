a7351e902be1fbade7cb004fcf8a4f0664ff4629
tyage <namatyage@gmail.com>
2015-04-23 22:33:10 +0900

    save channnel history

--- logger.rb
+++ logger.rb
@@ -1,15 +1,36 @@
 require './slack'
 require './db'
 
+# log history
+def fetch_history(channel)
+  Slack.channels_history(
+    channel: channel,
+    count: 1000
+  )['messages'].each do |mes|
+    SlackLog.create(
+      text: mes['text'],
+      posted_at: mes['ts'].to_f,
+      ts: mes['ts'],
+      channel: channel,
+      user: mes['user']
+    )
+  end
+end
+
+Slack.channels_list['channels'].each do |c|
+  fetch_history(c['id'])
+end
+
+# realtime logging
 realtime = Slack.realtime
 realtime.on :message do |mes|
   p mes
-  log = SlackLog.new
-  log.text = mes['text']
-  log.posted_at = mes['ts'].to_f
-  log.ts = mes['ts']
-  log.channel = mes['channel']
-  log.user = mes['user']
-  log.save
+  SlackLog.create(
+    text: mes['text'],
+    posted_at: mes['ts'].to_f,
+    ts: mes['ts'],
+    channel: mes['channel'],
+    user: mes['user']
+  )
 end
 realtime.start
