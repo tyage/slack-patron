7f7f8704ff06726909ab9c338845a53c99987598
tyage <namatyage@gmail.com>
2015-04-26 16:37:59 +0900

    fixed src and dist dir

--- gulpfile.js
+++ gulpfile.js
@@ -3,10 +3,10 @@ var babel = require('gulp-babel');
 var concat = require('gulp-concat');
 
 gulp.task('babel', function() {
-  return gulp.src('public/js/es6/*.js')
+  return gulp.src('public/src/js/*.js')
     .pipe(babel())
     .pipe(concat('app.js'))
-    .pipe(gulp.dest('public/js/'));
+    .pipe(gulp.dest('public/dist/js/'));
 });
 
 gulp.task('default', ['babel'], function() {
