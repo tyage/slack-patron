d7d5ebc6282a1dd9f5331da0923fc7bfbfcefa09
tyage <namatyage@gmail.com>
2015-04-26 16:46:33 +0900

    gulp less task

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var babel = require('gulp-babel');
 var concat = require('gulp-concat');
+var less = require('gulp-less');
 
 gulp.task('babel', function() {
   return gulp.src('public/src/js/*.js')
@@ -9,5 +10,12 @@ gulp.task('babel', function() {
     .pipe(gulp.dest('public/dist/js/'));
 });
 
-gulp.task('default', ['babel'], function() {
+gulp.task('less', function () {
+  return gulp.src('public/src/css/*.less')
+    .pipe(less())
+    .pipe(concat('app.css'))
+    .pipe(gulp.dest('public/dist/css'));
+});
+
+gulp.task('default', ['babel', 'less'], function() {
 });
