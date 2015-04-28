72890e4495736a99bcd48b43ef1917d3128764a9
tyage <namatyage@gmail.com>
2015-04-28 21:07:13 +0900

    update if user or channel has changed

--- logger.rb
+++ logger.rb
@@ -2,18 +2,26 @@ require './lib/slack'
 require './lib/db'
 
 # users
-User.delete_all
-Slack.users_list['members'].each do |u|
-  User.load_data(u)
+def update_users
+  User.delete_all
+  Slack.users_list['members'].each do |u|
+    User.load_data(u)
+  end
 end
+
+update_users
 
 p 'loading users finished!'
 
 # channels
-Channel.delete_all
-Slack.channels_list['channels'].each do |c|
-  Channel.load_data(c)
+def update_channels
+  Channel.delete_all
+  Slack.channels_list['channels'].each do |c|
+    Channel.load_data(c)
+  end
 end
+
+update_channels
 
 p 'loading channels finished!'
 
@@ -34,10 +42,32 @@ end
 
 p 'loading messages finished!'
 
-# log messages in realtime
+# realtime events
 realtime = Slack.realtime
+
 realtime.on :message do |m|
   p m
   SlackLog.load_data(m)
 end
+
+realtime.on :team_join do |e|
+  p "new user has joined"
+  update_users
+end
+
+realtime.on :user_change do |e|
+  p "user data has changed"
+  update_users
+end
+
+realtime.on :channel_created do |c|
+  p "channel has created"
+  update_channels
+end
+
+realtime.on :channel_rename do |c|
+  p "channel has renamed"
+  update_channels
+end
+
 realtime.start
