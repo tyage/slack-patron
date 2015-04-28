dbbcedd87310aa04ea4f54b14966656cca697e98
tyage <namatyage@gmail.com>
2015-04-28 17:38:29 +0900

    added raw_json to slack_logs

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150428083042) do
+ActiveRecord::Schema.define(version: 20150428083756) do
 
   create_table "channels", force: :cascade do |t|
     t.string   "slack_id"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20150428083042) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "ts"
+    t.text     "raw_json"
   end
 
   create_table "users", force: :cascade do |t|
