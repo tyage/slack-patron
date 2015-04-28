2b536827dbb567bc68356f94e58f28ff7c0ad408
tyage <namatyage@gmail.com>
2015-04-28 18:07:20 +0900

    import users, channels

--- import.rb
+++ import.rb
@@ -2,6 +2,46 @@ require './lib/db'
 require 'zip'
 require 'fileutils'
 
+def importMessages(channel, messages)
+  messages.each do |mes|
+    SlackLog.create(
+      text: mes['text'],
+      posted_at: mes['ts'].to_f,
+      ts: mes['ts'],
+      channel: channel.slack_id,
+      user: mes['user'],
+      raw_json: mes.to_json
+    )
+  end
+end
+
+def importChannels(channels)
+  Channel.delete_all
+  channels.each do |channel|
+    Channel.create(
+      slack_id: channel['id'],
+      name: channel['name'],
+      created: channel['created'],
+      creator: channel['creator'],
+      is_archived: channel['is_archived'],
+      raw_json: channel.to_json
+    )
+  end
+end
+
+def importUsers(users)
+  User.delete_all
+  users.each do |user|
+    User.create(
+      slack_id: user['id'],
+      name: user['name'],
+      is_bot: user['is_bot'],
+      image: user['profile']['image_32'],
+      raw_json: user.to_json
+    )
+  end
+end
+
 # format of exported file
 #
 # exported.zip
@@ -13,18 +53,6 @@ require 'fileutils'
 
 exportFile = ARGV[0]
 
-def importJson(channel, content)
-  JSON.parse(content).each do |mes|
-    SlackLog.create(
-      text: mes['text'],
-      posted_at: mes['ts'].to_f,
-      ts: mes['ts'],
-      channel: channel['id'],
-      user: mes['user']
-    )
-  end
-end
-
 dist = 'tmp/'
 begin
   Zip::File.open(exportFile) do |zip|
@@ -32,17 +60,18 @@ begin
       entry.extract(dist + entry.to_s)
     end
     open(dist + 'channels.json') do |io|
-      channels = JSON.load(io)
-      zip.each do |entry|
-        # channel/2015-01-01.json
-        if !File.directory?(dist + entry.to_s) and entry.to_s.split('/').size > 1
-          puts "import #{entry.to_s}"
-          channel = channels.find do |c|
-            entry.to_s.split('/')[0] == c['name']
-          end
-          content = entry.get_input_stream.read
-          importJson(channel, content)
-        end
+      importChannels(JSON.load(io))
+    end
+    open(dist + 'users.json') do |io|
+      importUsers(JSON.load(io))
+    end
+    zip.each do |entry|
+      # channel/2015-01-01.json
+      if !File.directory?(dist + entry.to_s) and entry.to_s.split('/').size > 1
+        puts "import #{entry.to_s}"
+        channel = Channel.where(name: entry.to_s.split('/')[0]).first
+        content = JSON.load(entry.get_input_stream)
+        importMessages(channel, content)
       end
     end
   end
