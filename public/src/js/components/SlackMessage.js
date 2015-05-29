b7d5eb59682336f5150ac3ab1aa1080c8250fccc
tyage <namatyage@gmail.com>
2015-05-29 14:58:15 +0900

    format for user link, channel link, special command

--- public/src/js/components/SlackMessage.js
+++ public/src/js/components/SlackMessage.js
@@ -1,23 +1,31 @@
 import React from 'react';
 
 export default React.createClass({
-  user() {
-    return this.props.users[this.props.message.user];
-  },
   formatDate(date) {
     return new Date(date * 1000).toLocaleString();
   },
   formatText(text) {
-    return text && text.replace(/<@[0-9A-Za-z]+\|([0-9A-Za-z]+)>/gi, "@$1");
+    let channelLink = (id) => `#${this.props.channels[id].name}`;
+    let userLink = (id) => `@${this.props.users[id].name}`;
+    let specialCommand = (command) => `@${command}`;
+    if (text) {
+      return text.replace(/<#([0-9A-Za-z]+)>/, (m, id) => channelLink(id))
+        .replace(/<#([0-9A-Za-z]+)\|([0-9A-Za-z]+)>/gi, (m, id) => channelLink(id))
+        .replace(/<@([0-9A-Za-z]+)>/, (m, id) => userLink(id))
+        .replace(/<@([0-9A-Za-z]+)\|([0-9A-Za-z]+)>/gi, (m, id) => userLink(id))
+        .replace(/<!(channel|everyone|group)>/gi, (m, command) => specialCommand(command));
+    }
+    return text;
   },
   render() {
+    let user = this.props.users[this.props.message.user];
     return (
       <div className="slack-message">
         <div className="slack-message-user-image">
-          <img src={this.user() && this.user().profile.image_32} />
+          <img src={user && user.profile.image_32} />
         </div>
         <div className="slack-message-content">
-          <div className="slack-message-user-name">{this.user() && this.user().name}</div>
+          <div className="slack-message-user-name">{user && user.name}</div>
           <div className="slack-message-date">{this.formatDate(this.props.message.ts)}</div>
           <div className="slack-message-text">{this.formatText(this.props.message.text)}</div>
         </div>
