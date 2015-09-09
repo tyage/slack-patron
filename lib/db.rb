09c50616d29d44ab90baf667e3a6e5e5ac18324b
tyage <namatyage@gmail.com>
2015-09-09 12:11:44 +0900

    fixed bug in replace_{channels, users} at symbol params

--- lib/db.rb
+++ lib/db.rb
@@ -8,7 +8,7 @@ db = Mongo::Client.new([ db_config['uri'] ], database: db_config['database'])
 Users = db['users']
 Users.indexes.create_one({ :id => 1 }, :unique => true)
 def replace_users(users)
-  ids = users.map{ |users| users[:id] }
+  ids = users.map{ |user| user['id'] }
   Users.find(id: { '$in' => ids }).delete_many
   Users.insert_many(users)
 end
@@ -16,7 +16,7 @@ end
 Channels = db['channels']
 Channels.indexes.create_one({ :id => 1 }, :unique => true)
 def replace_channels(channels)
-  ids = channels.map{ |channel| channel[:id] }
+  ids = channels.map{ |channel| channel['id'] }
   Channels.find(id: { '$in' => ids }).delete_many
   Channels.insert_many(channels)
 end
