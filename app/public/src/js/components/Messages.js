35439f5141de749cc7ec11b09b4b703915f2f3fc
tyage <namatyage@gmail.com>
2015-12-19 01:00:57 +0900

    show channel messages when channel changed

--- app/public/src/js/components/Messages.js
+++ app/public/src/js/components/Messages.js
@@ -1,14 +1,39 @@
 import React from 'react';
 import ChannelMessages from './ChannelMessages';
 import SlackActions from '../actions/SlackActions';
+import SlackCurrentChannelStore from '../stores/SlackCurrentChannelStore';
+
+const channelMessageType = Symbol();
+let getState = () => {
+  return {
+    messagesType: channelMessageType
+  };
+};
 
 export default React.createClass({
+  _onCurrentChannelChange() {
+    this.setState({
+      messagesType: channelMessageType
+    });
+  },
+  getInitialState() {
+    return getState();
+  },
   componentDidMount() {
+    SlackCurrentChannelStore.addChangeListener(this._onCurrentChannelChange);
   },
   render() {
+    let messages = () => {
+      switch (this.state.messagesType) {
+        case channelMessageType:
+          return <ChannelMessages />
+          break;
+        default:
+      }
+    }
     return (
       <div className="messages">
-        <ChannelMessages />
+        { messages() }
       </div>
     );
   }
