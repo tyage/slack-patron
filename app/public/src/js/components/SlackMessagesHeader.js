8f147f16f8758911a8a45ba9a9b27c68edc691ae
tyage <namatyage@gmail.com>
2015-12-18 12:10:12 +0900

    delete debug code

--- app/public/src/js/components/SlackMessagesHeader.js
+++ app/public/src/js/components/SlackMessagesHeader.js
@@ -22,7 +22,6 @@ export default React.createClass({
   },
   render() {
     let channel = _.find(this.state.channels, (c) => c.id === this.state.currentChannel);
-    console.log(channel && channel.name);
     return (
       <div className="slack-messages-header">
         <div className="channel-name">{channel && channel.name}</div>
