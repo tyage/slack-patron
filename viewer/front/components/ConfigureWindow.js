6669aae31e83f9ad884fa39d27ecabde6a3232a5
tyage <namatyage@gmail.com>
2017-09-27 16:40:04 +0900

    add this binding

--- viewer/front/components/ConfigureWindow.js
+++ viewer/front/components/ConfigureWindow.js
@@ -19,7 +19,7 @@ class ConfigureWindow extends React.Component {
             <p className="section-title">Import Slack Backup File</p>
             <p>Import a Slack backup file</p>
             <p>You can download Slack backup file from <a href={`https://${this.props.teamInfo.domain}.slack.com/services/export`} target="_blank">this page</a>(You need administrator privileges)</p>
-            <form id="import-slack-backup-form" onSubmit={this.importBackup} ref="importBackup">
+            <form id="import-slack-backup-form" onSubmit={this.importBackup.bind(this)} ref="importBackup">
               <div className="form-section">
                 <input type="file" name="file" ref="backupFile" />
               </div>
