5f639de088c5b43ad4ac349476269331893885f7
Yosshi999 <Yosshi999@users.noreply.github.com>
2020-03-04 15:27:00 +0000

    remove unnecessary code

--- elasticsearch-setup/setup.sh
+++ elasticsearch-setup/setup.sh
@@ -1,26 +1,5 @@
 #!/bin/sh
 
-#until $(curl -XPUT 'http://elasticsearch:9200/slack_logger' -d '{
-# "settings": {
-#   "index": {
-#     "analysis": {
-#       "tokenizer": {
-#         "kuromoji_user_dict": {
-#	   "type": "kuromoji_tokenizer"
-#	 }
-#       },
-#       "analyzer": {
-#         "analyzer": {
-#	   "type": "custom",
-#	   "tokenizer": "kuromoji_user_dict"
-#         }
-#       }
-#     }
-#   }
-# }
-#}' -o /tmp/output.txt); do
-#  sleep 5;
-#done
 until $(curl -XPUT 'http://elasticsearch:9200/slack_logger' -d '{
  "settings": {
    "analysis": {
