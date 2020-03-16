3e24e9b20b1efa1f9dc8eb82f16c908d1e5999c5
Koki Takahashi <hakatasiloving@gmail.com>
2020-03-16 15:51:23 +0900

    Make clusters work without setups

--- connector/run.sh
+++ connector/run.sh
@@ -1,9 +1,30 @@
 #!/bin/bash
 
-echo "hello"
-
-sleep 40
+mongo mongo:27017 <<< "rs.initiate()"
+curl -XPUT 'http://elasticsearch:9200/slack_logger' -d '{
+  "settings": {
+    "analysis": {
+      "analyzer": {
+        "kuromoji_analyzer": {
+          "type": "custom",
+          "tokenizer": "kuromoji_tokenizer"
+        }
+      }
+    }
+  },
+  "mappings": {
+    "messages": {
+      "properties": {
+        "text": {
+          "type": "text",
+          "analyzer": "kuromoji_analyzer"
+        },
+        "ts": {
+          "type": "double"
+        }
+      }
+    }
+  }
+}'
 
 mongo-connector -c /conf/config.json
-
-cat /var/log/mongo-connector/mongo-connector.log
